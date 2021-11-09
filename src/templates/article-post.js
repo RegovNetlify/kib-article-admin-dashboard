f029600a89d932fa83650198607d7e16604e6c61
Ali <ali.mirghafouri@gmail.com>
2021-11-09 16:14:29 +0800

    article test

---
+++ src/templates/article-post.js
@@ -0,0 +1,82 @@
+import React from "react";
+import Layout from "../components/Layout";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
+import { graphql, Link } from "gatsby";
+import Content, { HTMLContent } from "../components/Content";
+import { kebabCase } from "lodash";
+
+export const ArticlePostTemplate = ({
+  content,
+  contentComponent,
+  description,
+  title,
+  helmet,
+}) => {
+  const PostContent = contentComponent || Content;
+
+  return (
+    <section className="section">
+      {helmet || ""}
+      <div className="container content">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+              {title}
+            </h1>
+            <p>{description}</p>
+            <PostContent content={content} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+ArticlePostTemplate.propTypes = {
+  content: PropTypes.node.isRequired,
+  contentComponent: PropTypes.func,
+  description: PropTypes.string,
+  title: PropTypes.string,
+  helmet: PropTypes.object,
+};
+
+const ArticlePost = ({ data }) => {
+  const { markdownRemark: post } = data;
+
+  return (
+    <Layout>
+      <ArticlePostTemplate
+        content={post.html}
+        contentComponent={HTMLContent}
+        description={post.frontmatter.description}
+        helmet={
+          <Helmet titleTemplate="%s | Blog">
+            <title>{`${post.frontmatter.title}`}</title>
+            <meta
+              name="description"
+              content={`${post.frontmatter.description}`}
+            />
+          </Helmet>
+        }
+        title={post.frontmatter.title}
+      />
+    </Layout>
+  );
+};
+
+export default ArticlePost;
+
+export const articlePageQuery = graphql`
+  query ArticlePostByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+        description
+      }
+    }
+  }
+`;
