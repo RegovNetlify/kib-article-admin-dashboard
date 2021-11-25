31412af30336e9b5539668290af5ef779e4252e8
Ali <ali.mirghafouri@gmail.com>
2021-11-25 10:48:32 +0800

    link

--- src/components/recersiveNestedLinks.js
+++ src/components/recersiveNestedLinks.js
@@ -1,23 +1,36 @@
-import React from "react"
+import React from "react";
 
 export const RecursiveNestedLink = (props) => {
-    return (
-        <>
-            {props.links.map(link => {
-                return(
-                    <>
-                    <a href={link.link} className={`footer-info-link ${link.link === ''?"disabled":""}`} target={link.target ?"_blank":"_self"} rel="noreferrer">
-                  {link.name}
-                </a>
-                    {link.subLink !== undefined && 
-                    <div className={`footer_info_subLink ${props.open?"open":""}`}>
-                    <RecursiveNestedLink
-                    links={link.subLink} open={props.open}/>
-                    </div>
-                    }
-                    </>
-                )
-            })}
-        </>
-    )
-}
+  const isBrowser = typeof window !== "undefined";
+  const handleLink = (link) => {
+    if (isBrowser) {
+      sessionStorage.clear();
+      window.location.href = link;
+    }
+  };
+  return (
+    <>
+      {props.links.map((link) => {
+        return (
+          <>
+            <div
+              onClick={() => handleLink(link.link)}
+              className={`cursor_pointer footer-info-link ${
+                link.link === "" ? "disabled" : ""
+              }`}
+            >
+              {link.name}
+            </div>
+            {link.subLink !== undefined && (
+              <div
+                className={`footer_info_subLink ${props.open ? "open" : ""}`}
+              >
+                <RecursiveNestedLink links={link.subLink} open={props.open} />
+              </div>
+            )}
+          </>
+        );
+      })}
+    </>
+  );
+};
