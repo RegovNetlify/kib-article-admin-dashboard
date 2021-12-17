a3505d34554486c74475cb6ef6d2e628b6e1fa38
Ali <ali.mirghafouri@gmail.com>
2021-12-17 11:17:36 +0800

    fix

--- netlify/functions/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded.js
@@ -1,8 +1,24 @@
+import axios from "axios";
+
+export const API_Call = async (url, data, options) => {
+  let response;
+  try {
+    response = await axios.post(url, data, options);
+  } catch (error) {
+    response = null;
+  }
+  return response;
+};
+
+export const updateArticle = async () => {
+  return await API_Call(
+    "https://api.regovitservices.com/v1/api/app/updatearticle"
+  );
+};
+
 exports.handler = async function (event, context) {
   // your server-side functionality
-  fetch("https://api.regovitservices.com/v1/api/app/updatearticle").then(
-    (res) => {
-      console.log(res);
-    }
-  );
+  updateArticle().then((res) => {
+    console.log(res);
+  });
 };
